1e8e6b78a4bdf110f975f38c5fa00411b384bf30
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-10-14 23:08:50 +0530

    init

---
+++ src/main/java/com/ksb/micro/profile_photo/ProfilePhotoApplication.java
@@ -0,0 +1,13 @@
+package com.ksb.micro.profile_photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilePhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilePhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksb/micro/profile_photo/ProfilePhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksb.micro.profile_photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilePhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
