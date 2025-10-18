b5bdf525d968959f34c3fe233b9b407a45b09181
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-10-18 18:03:30 +0530

    api structure changed, request mapping updated

--- src/main/java/com/ksb/micro/profile_photo/ProfilePhotoApplication.java
+++ src/main/java/com/ksb/micro/profile_photo/ProfilePhotoApplication.java
@@ -1,9 +1,15 @@
 package com.ksb.micro.profile_photo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+/*@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.ksb.micro.profile_photo")*/
+@SpringBootApplication(scanBasePackages = "com.ksb.micro.profile_photo")
 public class ProfilePhotoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ksb/micro/profile_photo/controller/ProfilePhotoController.java
+++ src/main/java/com/ksb/micro/profile_photo/controller/ProfilePhotoController.java
@@ -1,7 +1,7 @@
-package com.ksb.micro.user_profile.controller;
+package com.ksb.micro.profile_photo.controller;
 
-import com.ksb.micro.user_profile.model.ProfilePhoto;
-import com.ksb.micro.user_profile.service.impl.ProfilePhotoService;
+import com.ksb.micro.profile_photo.model.ProfilePhoto;
+import com.ksb.micro.profile_photo.service.impl.ProfilePhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,13 +14,13 @@ import java.io.IOException;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/banks/{bankId}/users/{userId}/photo")
+@RequestMapping("/v1/banks/{bankId}/users/{userId}")
 public class ProfilePhotoController {
 
     @Autowired
     private ProfilePhotoService profilePhotoService;
 
-    @GetMapping
+    @GetMapping(value = "/photo")
     public ResponseEntity<byte[]> getProfilePhotoByUserId(@PathVariable Long userId){
         Optional<ProfilePhoto> photo = profilePhotoService.getProfilePhotoByUserId(userId);
         if(photo.isPresent()){
@@ -32,7 +32,7 @@ public class ProfilePhotoController {
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, value = "/photo")
     public ResponseEntity<String> uploadProfilePhoto(@PathVariable Long userId, @RequestParam("file") MultipartFile file) throws IOException{
         Optional<ProfilePhoto> existingPhoto = profilePhotoService.getProfilePhotoByUserId(userId);
         if(existingPhoto.isPresent()){
